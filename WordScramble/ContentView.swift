caeebe199c5fc470e90b05255d31c80a8d084949
walker-sean <walker.se@northeastern.edu>
2023-08-30 15:52:59 -0400

    Testing out UITextCheckers and stuff

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,21 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
-    
-    func loadFile() {
-        if let fileUrl = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileUrl) {
-                
-            }
-        }
-    }
+    func whatevs() {
+        let word = "swift"
+        let checker = UITextChecker()
         
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+    }
+    
     
     var body: some View {
-        List(people, id: \.self) {
-            Text($0)
-        }
+        Text("\(NSNotFound)")
     }
 }
 
