ed4244767b82ea7535bc4628175045e00a2d444f
walker-sean <walker.se@northeastern.edu>
2023-08-30 14:58:09 -0400

    Learning about List

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(people, id: \.self) {
+            Text($0)
         }
-        .padding()
     }
 }
 
